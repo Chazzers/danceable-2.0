8d6d6459e53e9822060fde57996aa1fc2e0fd8cf
Chazzers <https://ChazzM@bitbucket.org>
2021-05-08 20:49:52 +0200

    fix offline

--- public/sw.js
+++ public/sw.js
@@ -1,50 +1,57 @@
 const cacheItems = [
 	'/manifest.json',
 	'/sw.js',
-	'/js/client.js',
+	'/dist/js/client.js',
 	'/images/music-icon.png',
 	'/images/icons/icon-192x192.png',
 	'/images/icons/icon-256x256.png',
 	'/images/icons/icon-384x384.png',
 	'/images/icons/icon-512x512.png',
-	'/fonts/Poppins-Black.woff',
-	'/fonts/Poppins-Black.woff2',
-	'/fonts/Poppins-Bold.woff',
-	'/fonts/Poppins-Bold.woff2',
-	'/fonts/Poppins-Light.woff',
-	'/fonts/Poppins-Light.woff2',
-	'/fonts/Poppins-Medium.woff',
-	'/fonts/Poppins-Medium.woff2',
-	'/fonts/Poppins-Regular.woff',
-	'/fonts/Poppins-Regular.woff2',
-	'/css/styles.css',
+	'/dist/fonts/Poppins-Black.woff',
+	'/dist/fonts/Poppins-Black.woff2',
+	'/dist/fonts/Poppins-Bold.woff',
+	'/dist/fonts/Poppins-Bold.woff2',
+	'/dist/fonts/Poppins-Light.woff',
+	'/dist/fonts/Poppins-Light.woff2',
+	'/dist/fonts/Poppins-Medium.woff',
+	'/dist/fonts/Poppins-Medium.woff2',
+	'/dist/fonts/Poppins-Regular.woff',
+	'/dist/fonts/Poppins-Regular.woff2',
+	'/dist/css/styles.css',
 	'/playlists'
 ]
 
 self.addEventListener('install', (event) => {
-	event.waitUntil(
+	return event.waitUntil(
 		caches.open('caches')
 			.then(cache => cache.addAll(cacheItems))
-			.then(() => self.skipWaiting())
 	)
 })
 
 self.addEventListener('fetch', (event) => {
-	event.respondWith(
-		caches.match(event.request)
-			.then((response) => {
-				if(response) {
-					return response
-				}
-				return fetch(event.request)
-					.then(res => res)
-					.catch(() => caches.open('caches')
-						.then(cache => {
-							return cache.match('/playlists')
-						})
-					)}))
+	return event.respondWith(
+		caches.open('caches')
+			.then(cache => cache.match(event.request)
+				.then(() => fetch(event.request)
+					.catch(() => caches.match(event.request)
+						.catch(() => caches.match('/playlists'))
+					)
+				)
+			)
+	)
 })
 
 self.addEventListener('activate', (event) => {
-	event.waitUntil(clients.claim())
+	const cacheAllowlist = ['caches']
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (cacheAllowlist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName)
+					}
+				})
+			)
+		})
+	)
 })
