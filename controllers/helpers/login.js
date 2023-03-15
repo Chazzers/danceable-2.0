6af10ee89dd15aa28169b519aadb0438bc07d437
Chazzers <chazz567@gmail.com>
2023-03-15 11:59:35 +0100

    change url and add prettierrc

--- controllers/helpers/login.js
+++ controllers/helpers/login.js
@@ -1,22 +1,23 @@
-const generateRandomString = require('../helpers/generateRandomString.js')
-const queryString = require('query-string')
+const generateRandomString = require('../helpers/generateRandomString.js');
+const queryString = require('query-string');
 
 function login(req, res) {
-	const responseType = 'code'
-	const stateKey = 'spotify_auth_state'
-	const scope = 'user-read-private user-read-email playlist-read-private'
-	const urlAuthorize = 'https://accounts.spotify.com/authorize'
-	const state = generateRandomString(16)
+  const responseType = 'code';
+  const stateKey = 'spotify_auth_state';
+  const scope = 'user-read-private user-read-email playlist-read-private';
+  const urlAuthorize = 'https://accounts.spotify.com/authorize';
+  const state = generateRandomString(16);
 
-	res.cookie(stateKey, state)
+  res.cookie(stateKey, state);
+  const url = `${urlAuthorize}?${queryString.stringify({
+    response_type: responseType,
+    client_id: process.env.CLIENT_ID,
+    scope: scope,
+    redirect_uri: process.env.REDIRECT_URI,
+    state: state,
+  })}`;
 
-	return res.redirect(`${urlAuthorize}?${queryString.stringify({
-		response_type: responseType,
-		client_id: process.env.CLIENT_ID,
-		scope: scope,
-		redirect_uri: process.env.REDIRECT_URI,
-		state: state
-	})}`)
+  return res.redirect(url);
 }
 
-module.exports = login
+module.exports = login;
